e252e723446d9a2b6da0bf59d734ca633f52a201
ericgendaneza <ericgendaneza0@gmail.com>
2025-03-17 20:28:34 +0200

    Update python.py

--- python.py
+++ python.py
@@ -1,15 +1,15 @@
- #print("hello world")
-# string = input("enter a string :")
-# print(len(string))
-# string=input("enter any srting").replace(" ","")
-# print(len(string))
-# words=[]
-# for i in range(5):
-#     word = input(f"Enter word {i+1}: ")
-#     words.append(word)
-#     print("\nWord Lengths:")
-# for word in words:
-#     print(f"'{word}' is {len(word)} characters")
+ print("hello world")
+string = input("enter a string :")
+print(len(string))
+string=input("enter any srting").replace(" ","")
+print(len(string))
+words=[]
+for i in range(5):
+    word = input(f"Enter word {i+1}: ")
+    words.append(word)
+    print("\nWord Lengths:")
+for word in words:
+    print(f"'{word}' is {len(word)} characters")
 arr1 = ['a', 'i', 'o', 'e', 'u']
 arr2 = ['k', 'j', 'p', 't', 'f']
 
